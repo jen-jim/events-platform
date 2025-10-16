4481639405c278a0d1d1972e5bd42d6b60a6e652
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-10-16 18:10:20 +0100

    make nav bar responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { CircleUser } from "lucide-react";
+import { CircleUser, Coffee } from "lucide-react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext.tsx";
@@ -10,20 +10,27 @@ export function Navbar() {
     return (
         <header className="site-header">
             <nav className="navbar">
-                <Link to="/" className="navbar-logo">
-                    The Cupping Room
-                </Link>
-
-                <p className="navbar-tagline">
-                    Brewed experiences, shared together
-                </p>
+                <div className="navbar-left">
+                    <Link to="/" className="navbar-logo">
+                        The Cupping Room <Coffee className="navbar-logo-icon" />
+                    </Link>
+                    <p className="navbar-tagline">
+                        Brewed experiences, shared together
+                    </p>
+                </div>
 
                 <div className="navbar-links">
                     {user ? (
                         <>
-                            <Link to="/profile" className="navbar-user">
-                                <CircleUser size={18} />{" "}
-                                <span>{user.name}</span>
+                            <Link
+                                to="/profile"
+                                className="navbar-user"
+                                title="Profile"
+                            >
+                                <CircleUser className="navbar-icon" />{" "}
+                                <span className="navbar-username">
+                                    {user.name}
+                                </span>
                             </Link>
                         </>
                     ) : (
